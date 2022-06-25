c6033407e1aa5ad21e52201a441ed5bab7de6e0f
KENTA-AGAWA1984 <justbeforethewarwith100mile@gmail.com>
2022-06-25 23:48:24 +0900

    商品コントローラーにnewアクションを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  get 'items/index'
   root to: "items#index"
-  resources :users, only: [:edit, :update]
-  resources :items, only: [:index, :new]
+  resources :users, only: [:edit, :update] 
+  resources :items, only: [:index, :new, :create]
 end
+  
